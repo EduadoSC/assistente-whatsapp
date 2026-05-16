39d2401ccbcb183c7f6984d6e3b370406e76b3dd
Renato Ceadareanu <renatoceadareanu@gmail.com>
2026-05-16 15:27:43 -0300

    fix conexao banco railway

--- db.js
+++ db.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-const pool = mysql.createPool(process.env.MYSQL_URL).promise();
+const pool = mysql.createPool({
+    uri: process.env.MYSQL_URL,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+}).promise();
 
 module.exports = pool;
