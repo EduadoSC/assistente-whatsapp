e7aa874e08b4107c29a29478724abd8a2f52299c
Renato Ceadareanu <renatoceadareanu@gmail.com>
2026-05-16 14:49:58 -0300

    config mysql para deploy

--- db.js
+++ db.js
@@ -1,15 +1,6 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,      // ← adiciona isso
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-}).promise();
+const pool = mysql.createPool(process.env.MYSQL_URL).promise();
 
 module.exports = pool;
